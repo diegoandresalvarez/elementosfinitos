ce6dca22e88286d25a64f11111901094d1598101
Diego Andrés Alvarez Marín <daalvarez@unal.edu.co>
2019-08-28 10:55:40 -0500

    Mejorando los códigos de Python

--- codigo/repaso_matricial/barra_1d/c1_ejemplo_barra_v1.py
+++ codigo/repaso_matricial/barra_1d/c1_ejemplo_barra_v1.py
@@ -1,28 +1,30 @@
-from sympy import *
+# -*- coding: utf-8 -*-
 
-# Se definen las variables simbolicas
-E, A, L, u3, u4, P = symbols('E A L u3 u4 P')
+import sympy as sp
+
+# Se definen las variables simbólicas
+E, A, L, u3, u4, P = sp.symbols('E A L u3 u4 P')
 
 # Se calculan las rigideces para cada barra
 k1 = E*A/L; k2 = E*A/L; k3 = 2*E*A/L
 
 # Se define la matriz de rigidez
-K = Matrix([
+K = sp.Matrix([
     [ k1,   0 ,  -k1      ,   0  ],
     [ 0 ,   k2,  -k2      ,   0  ],
     [-k1,  -k2,   k1+k2+k3,  -k3 ],
     [ 0 ,   0 ,  -k3      ,   k3 ]])
 
-# Se define el vector de desplazamientos nodales, teniendo en cuenta que 
+# Se define el vector de desplazamientos nodales, teniendo en cuenta que
 # u1=0 y u2=0
-a = Matrix([0, 0, u3, u4])  # SymPy lo toma como un vector columna
+a = sp.Matrix([0, 0, u3, u4])  # SymPy lo toma como un vector columna
 
 # Se define el vector de fuerzas nodales de equilibrio
-f = Matrix([ 0, 0, 0, P])
+f = sp.Matrix([ 0, 0, 0, P])
 
 # Se definen g.d.l. conocidos y desconocidos asociados a los desplazamientos
-c = Matrix([1, 2]) - ones(2,1);         d = Matrix([3, 4]) - ones(2,1)
-#c = Matrix([1, 2]).applyfunc(lambda x : x-1)
+c = sp.Matrix([1, 2]) - sp.ones(2,1);       d = sp.Matrix([3, 4]) - sp.ones(2,1)
+#c = sp.Matrix([1, 2]).applyfunc(lambda x : x-1)
 
 # Se descomponen los vectores a, f y la matriz K
 Kcc = K.extract(c,c);      Kcd = K.extract(c,d)
@@ -34,5 +36,5 @@ fc  = f.extract(d,[0]);    fd  = f.extract(c,[0])
 ad = Kdd.solve(fc - Kdc*ac)
 qd = Kcc*ac + Kcd*ad - fd
 
-pprint(ad)
-pprint(qd)
+sp.pprint(ad)
+sp.pprint(qd)
---
+++ codigo/repaso_matricial/barra_1d/c1_ejemplo_barra_v2.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+# Ejemplo 1.1. Oñate
+
+import numpy as np
+import sympy as sp
+
+def ensamblar(K, idx, Ke):
+    ''' Ensambla la matriz de rigidez local Ke en la matriz de rigidez global K
+
+    Uso:
+    ensamblar(K, idx, Ke)
+
+    Parametros de entrada:
+    K   -> matriz de rigidez global
+    idx -> grados de libertad donde debe proceder el ensamblaje
+    Ke  -> matriz de rigidez local
+    '''
+
+    # se verifican las dimensiones de las matrices
+    nfil, ncol = K.shape
+    assert nfil == ncol, "La matriz de rigidez K debe ser cuadrada"
+
+    nfil, ncol = Ke.shape
+    assert nfil == ncol == len(idx),\
+            "Ke debe ser cuadrada y debe tener el mismo número de filas que idx"
+
+    # se procede con el ensamblaje
+    for i in range(nfil):
+        for j in range(ncol):
+            K[idx[i], idx[j]] += Ke[i,j]
+
+# %% defino las variables
+E, A, L, P = sp.symbols('E A L P')         # define las variables simbolicas
+
+long = [L,  L,  L/2]                       # longitud de la barra
+
+# LaG: local a global: matriz que relaciona nodos locales y globales
+# (se lee la barra x va del nodo i al nodo j)
+LaG = np.array([[1, 3],         # fila = barra
+                [2, 3],         # col1 = nodo global asociado a nodo local 1
+                [3, 4]]) - 1    # col2 = nodo global asociado a nodo local 2
+
+k = [E*A/longitud for longitud in long]    # (k minuscula) rigidez de cada barra
+
+# %% ensamblo la matriz de rigidez global (K mayuscula)
+K = sp.zeros(4)       # separa memoria para matriz de rigidez global K
+for e in range(3):    # para cada una de las barras e = 1, 2 y 3
+   idx = LaG[e,:]     # extrae indices de los nodos globales de la barra e
+   Ke  = k[e]*np.array([[1, -1],[-1, 1]])  # matriz de rigidez local
+   ensamblar(K, idx, Ke)                   # ensamblaje matricial
+
+# %% grados de libertad del desplazamiento conocidos (c) y desconocidos (d)
+c = np.array([1, 2]) - 1;    d = np.setdiff1d(np.arange(4), c)
+c = sp.Matrix(c);            d = sp.Matrix(d)   # se convierten de Numpy a SymPy
+
+# %% extraigo las submatrices y especifico las cantidades conocidas
+# f = vector de fuerzas nodales equivalentes
+# q = vector de fuerzas nodales de equilibrio del elemento
+# a = desplazamientos
+
+#| qd |   | Kcc Kcd || ac |   | fd |     recuerde siempre que qc=0
+#|    | = |         ||    | - |    |
+#| qc |   | Kdc Kdd || ad |   | fc |     en este caso en particular fd=0
+Kcc = K.extract(c,c);      Kcd = K.extract(c,d)
+Kdc = K.extract(d,c);      Kdd = K.extract(d,d)
+
+ac = sp.Matrix([0, 0])   # SymPy los toma como vectores columna
+fc = sp.Matrix([0, P])
+
+# %% resuelvo el sistema de ecuaciones
+ad = Kdd.solve(fc - Kdc*ac)
+qd = Kcc*ac + Kcd*ad
+
+# %% formo los vectores de desplazamientos (a) y fuerzas (q)
+a = sp.zeros(4,1); q = sp.zeros(4,1)     # separo la memoria
+for i in range(len(c)):
+    a[c[i]] = ac[i]
+    q[c[i]] = qd[i]
+
+for i in range(len(d)):
+    a[d[i]] = ad[i]
+    # q[d[i]] = qc[i] = 0
+
+# %% calculo las cargas axiales en cada barra
+N = sp.zeros(3,1)
+for e in range(3):
+   N[e] = k[e]*(a[LaG[e,2-1]] - a[LaG[e,1-1]])
+
+# %% imprimo los resultados
+print('\n\na = \n'); sp.pprint(a)
+print('\n\nq = \n'); sp.pprint(q)
+print('\n\nN = \n'); sp.pprint(N)
