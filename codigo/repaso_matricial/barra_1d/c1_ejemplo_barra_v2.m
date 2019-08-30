c9d19443b60c912760e86393787f13072fef6d53
Diego Andrés Alvarez Marín <diegotorquemada@gmail.com>
2019-08-30 08:09:55 -0500

    Imprimo K

--- codigo/repaso_matricial/barra_1d/c1_ejemplo_barra_v2.m
+++ codigo/repaso_matricial/barra_1d/c1_ejemplo_barra_v2.m
@@ -22,7 +22,9 @@ for e = 1:3           % para cada una de las barras e = 1, 2 y 3
 end
 
 disp('Imprimamos la matriz de rigidez =')   
-K
+sympref('AbbreviateOutput', false);
+pretty(K)
+sympref('AbbreviateOutput','default');
 
 %% grados de libertad del desplazamiento conocidos (c) y desconocidos (d)
 c = [1 2];    d = setdiff(1:4,c);
