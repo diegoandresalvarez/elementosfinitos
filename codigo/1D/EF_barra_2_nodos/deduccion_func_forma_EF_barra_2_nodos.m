619fe3d6dcc96f057b6b7812855d335eb0a657ea
Diego Andrés Alvarez Marín <daalvarez@unal.edu.co>
2019-09-09 21:21:08 -0500

    Código de deducciones en SymPy

--- codigo/1D/EF_barra_2_nodos/deduccion_func_forma_EF_barra_2_nodos.m
+++ codigo/1D/EF_barra_2_nodos/deduccion_func_forma_EF_barra_2_nodos.m
@@ -1,8 +1,8 @@
 %% Deducción de las funciones de forma del elemento de barra de 2 nodos
 
 syms u1 u2 x x1 x2 a1 a0;
-r = solve('u1 = a1*x1 + a0', ...
-          'u2 = a1*x2 + a0', 'a0','a1');
+r = solve(u1 == a1*x1 + a0, ...
+          u2 == a1*x2 + a0,     a0, a1);
 disp('a0 = '); pretty(r.a0)
 disp('a1 = '); pretty(r.a1)
 
