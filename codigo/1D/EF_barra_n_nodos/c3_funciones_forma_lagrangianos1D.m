825e95fb74c748a042d8df2d1427bae48775a40a
Diego Andrés Alvarez Marín <daalvarez@unal.edu.co>
2019-09-16 13:42:20 -0500

    Arreglando problema con legend()

--- codigo/1D/EF_barra_n_nodos/c3_funciones_forma_lagrangianos1D.m
+++ codigo/1D/EF_barra_n_nodos/c3_funciones_forma_lagrangianos1D.m
@@ -4,13 +4,13 @@ clear, clc, close all
 
 % Calculo las funciones de forma
 syms xi
-N1 = factor(poly2sym(polyfit([-1 1],[1 0],1),xi));
-N2 = factor(poly2sym(polyfit([-1 1],[0 1],1),xi));
+N1 = poly2sym(polyfit([-1 1],[1 0],1),xi);
+N2 = poly2sym(polyfit([-1 1],[0 1],1),xi);
 
 % Imprimo las funciones de forma
 fprintf('\n\nFunciones de Forma Lagrangianas de DOS nodos:\n')
-fprintf('\n\nN1 = '), pretty(N1)
-fprintf('\n\nN2 = '), pretty(N2)
+fprintf('\n\nN1 = \n'), pretty(N1)
+fprintf('\n\nN2 = \n'), pretty(N2)
 
 % Grafico las funciones de forma
 figure                 % Creo un lienzo
@@ -29,15 +29,15 @@ plot([-1 1],[0 0], 'ko', [-1 1],[1 1], 'ko') % grafico los nodos
 
 % Calculo las funciones de forma
 syms xi
-N1 = factor(poly2sym(polyfit([-1 0 1],[1 0 0],2),xi)); % = xi*(xi-1)/2
-N2 = factor(poly2sym(polyfit([-1 0 1],[0 1 0],2),xi)); % = (1+xi)*(1-xi)
-N3 = factor(poly2sym(polyfit([-1 0 1],[0 0 1],2),xi)); % = xi*(xi+1)/2
+N1 = poly2sym(polyfit([-1 0 1],[1 0 0],2),xi); % = xi*(xi-1)/2
+N2 = poly2sym(polyfit([-1 0 1],[0 1 0],2),xi); % = (1+xi)*(1-xi)
+N3 = poly2sym(polyfit([-1 0 1],[0 0 1],2),xi); % = xi*(xi+1)/2
 
 % Imprimo las funciones de forma
 fprintf('\n\nFunciones de Forma Lagrangianas de TRES nodos:\n')
-fprintf('\n\nN1 = '), pretty(N1)
-fprintf('\n\nN2 = '), pretty(N2)
-fprintf('\n\nN3 = '), pretty(N3)
+fprintf('\n\nN1 = \n'), pretty(N1)
+fprintf('\n\nN2 = \n'), pretty(N2)
+fprintf('\n\nN3 = \n'), pretty(N3)
 
 % Grafico las funciones de forma
 figure                 % Creo un lienzo
@@ -57,15 +57,15 @@ plot([-1 0 1],[0 0 0], 'ko', [-1 0 1],[1 1 1], 'ko') % grafico los nodos
 
 % Calculo las funciones de forma
 syms xi
-N1 = factor(poly2sym(polyfit([-1 -1/3 1/3 1],[1 0 0 0],3),xi));
-N2 = factor(poly2sym(polyfit([-1 -1/3 1/3 1],[0 1 0 0],3),xi));
-N3 = factor(poly2sym(polyfit([-1 -1/3 1/3 1],[0 0 1 0],3),xi));
-N4 = factor(poly2sym(polyfit([-1 -1/3 1/3 1],[0 0 0 1],3),xi));
+N1 = poly2sym(polyfit([-1 -1/3 1/3 1],[1 0 0 0],3),xi);
+N2 = poly2sym(polyfit([-1 -1/3 1/3 1],[0 1 0 0],3),xi);
+N3 = poly2sym(polyfit([-1 -1/3 1/3 1],[0 0 1 0],3),xi);
+N4 = poly2sym(polyfit([-1 -1/3 1/3 1],[0 0 0 1],3),xi);
 
 % Imprimo las funciones de forma
 fprintf('\n\nFunciones de Forma Lagrangianas de CUATRO nodos:\n')
-fprintf('\n\nN1 = '), pretty(N1);  fprintf('\n\nN2 = '), pretty(N2)
-fprintf('\n\nN3 = '), pretty(N3);  fprintf('\n\nN4 = '), pretty(N4)
+fprintf('\n\nN1 = \n'), pretty(N1);  fprintf('\n\nN2 = \n'), pretty(N2)
+fprintf('\n\nN3 = \n'), pretty(N3);  fprintf('\n\nN4 = \n'), pretty(N4)
 
 % Grafico las funciones de forma
 figure                 % Creo un lienzo
@@ -75,10 +75,11 @@ h1 = ezplot(N1, [-1 1]); set(h1, 'Color', 'r', 'LineWidth', 2);
 h2 = ezplot(N2, [-1 1]); set(h2, 'Color', 'b', 'LineWidth', 2);   
 h3 = ezplot(N3, [-1 1]); set(h3, 'Color', 'c', 'LineWidth', 2);    
 h4 = ezplot(N4, [-1 1]); set(h4, 'Color', 'm', 'LineWidth', 2);    
-legend('N1(\xi)','N2(\xi)','N3(\xi)','N4(\xi)','Location','Best');
 title('Funciones de Forma Lagrangianas de CUATRO nodos')
 xlabel('\xi'); 
 ylabel('N_i(\xi)');
 plot([-1 -1/3 1/3 1],[0 0 0 0], 'ko', [-1 -1/3 1/3 1],[1 1 1 1], 'ko')
 axis([-1 1 -0.4 1.2])
+legend([h1 h2 h3 h4], 'N1(\xi)','N2(\xi)','N3(\xi)','N4(\xi)','Location','Best');
+
 
