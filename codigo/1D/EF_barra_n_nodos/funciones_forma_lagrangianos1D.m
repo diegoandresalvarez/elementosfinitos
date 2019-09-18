d876d97a976855744d37a07e0f5119b18285516c
Diego Andrés Alvarez Marín <daalvarez@unal.edu.co>
2019-09-17 22:53:06 -0500

    Organizando los errores de redondeo

--- codigo/1D/EF_barra_n_nodos/funciones_forma_lagrangianos1D.m
+++ codigo/1D/EF_barra_n_nodos/funciones_forma_lagrangianos1D.m
@@ -81,3 +81,37 @@ ylabel('N_i(\xi)');
 plot([-1 -1/3 1/3 1],[0 0 0 0], 'ko', [-1 -1/3 1/3 1],[1 1 1 1], 'ko')
 axis([-1 1 -0.4 1.2])
 legend([h1 h2 h3 h4], 'N1(\xi)','N2(\xi)','N3(\xi)','N4(\xi)','Location','Best');
+
+%% -------------------------------------------------------------------------
+%% Funciones de Forma Lagrangianas de cinco nodos
+
+% Calculo las funciones de forma
+syms xi
+N = cell(5,1);
+for i = 1:5
+    coef = polyfit([-1 -1/2 0 1/2 1], [i==1 i==2 i==3 i==4 i==5], 4);
+    coef(abs(coef) < 1e-10) = 0; % se eliminan los errores de redondeo
+    N{i} = poly2sym(coef, xi);     
+end                                  
+
+% Imprimo las funciones de forma
+fprintf('\n\nFunciones de Forma Lagrangianas de CINCO nodos:\n')
+for i = 1:5
+    fprintf('\n\nN%d = \n', i); pretty(N{i});
+end
+
+% Grafico las funciones de forma
+figure                 % Creo un lienzo
+grid on                % creo la rejilla
+hold on;               % Para que no se sobreescriban los graficos
+h = zeros(5,1);
+color = ['r', 'b', 'c', 'm', 'k'];
+for i = 1:5
+    h(i) = fplot(N{i}, [-1 1], 'Color', color(i), 'LineWidth', 2);
+end
+title('Funciones de Forma Lagrangianas de CINCO nodos')
+xlabel('\xi'); 
+ylabel('N_i(\xi)');
+plot([-1 -1/2 0 1/2 1],[0 0 0 0 0], 'ko', [-1 -1/2 0 1/2 1],[1 1 1 1 1], 'ko')
+axis([-1 1 -0.6 1.2])
+legend(h, 'N1(\xi)','N2(\xi)','N3(\xi)','N4(\xi)','N5(\xi)', 'Location','Best');
