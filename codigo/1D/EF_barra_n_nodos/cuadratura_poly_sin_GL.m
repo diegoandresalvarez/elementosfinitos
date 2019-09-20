30960ac5d06816b9a277c4a2413cb8f864efd0d6
Diego Andrés Alvarez Marín <daalvarez@unal.edu.co>
2019-09-19 22:25:25 -0500

    Mejorando la presentación

--- codigo/1D/EF_barra_n_nodos/cuadratura_poly_sin_GL.m
+++ codigo/1D/EF_barra_n_nodos/cuadratura_poly_sin_GL.m
@@ -2,16 +2,15 @@ clear, clc, close all
 
 % Integracion de:
 %
-% f(x) = 0.2 + 25*x - 200*x.^2 +675*x.^3 - 900*x.^4 + 400*x.^5
+f   = @(x) 0.2 + 25*x - 200*x.^2 +675*x.^3 - 900*x.^4 + 400*x.^5;
 %
 % entre 0 y 0.8 usando cuadraturas de Gauss-Legendre
 
-a   = 0;                  % Limites de integracion
+a   = 0;                  % limites de integracion
 b   = 0.8;        
-f   = @(x) 0.2 + 25*x - 200*x.^2 +675*x.^3 - 900*x.^4 + 400*x.^5; % la funcion
-sol = 3076/1875;
-err = zeros(10,1);        % Separo la memoria
-for m = 1:10;             % Vario el numero de puntos de la cuadratura
+sol = 3076/1875;          % solucion exacta
+err = zeros(10,1);        % separo la memoria
+for m = 1:10              % vario el numero de puntos de la cuadratura
    [x,c] = gausslegendre_quad(m);  % calculo w y c de la cuadratura
    err(m) = abs(((b-a)/2)*sum(c.*f((b+a)/2 + (b-a)*x/2)) - sol);
 end
@@ -25,21 +24,20 @@ grid                      % pongo la rejilla
 
 % Integracion de:
 %
-% f(x) = sin(x)
+f   = @(x) sin(x);
 %
 % entre 0 y pi/2 usando cuadraturas de Gauss-Legendre
 
-a   = 0;
+a   = 0;                  % limites de integracion
 b   = pi/2;
-f   = @(x) sin(x);
-sol = 1;
-err = zeros(10,1);
-for m = 1:10;
+sol = 1;                  % solucion exacta
+err = zeros(10,1);        % separo la memoria
+for m = 1:10
    [x,c] = gausslegendre_quad(m);
    err(m) = abs(((b-a)/2)*sum(c.*f((b+a)/2 + (b-a)*x/2)) - sol);
 end
 figure
-semilogy(err); % se grafica en escala logaritmica para apreciar el error
+semilogy(err); % escala logaritmica para apreciar mejor el error
 xlabel('Numero de puntos en la cuadratura');
 ylabel('Error');
 title('Cuadratura de Gauss Legendre');
