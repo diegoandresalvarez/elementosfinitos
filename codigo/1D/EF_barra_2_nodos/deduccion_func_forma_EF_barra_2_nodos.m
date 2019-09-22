a2ba9929bb93123561436934882b6627e33dc925
Diego Andrés Alvarez Marín <daalvarez@unal.edu.co>
2019-09-21 21:42:39 -0500

    Mejorando el comando collect()

--- codigo/1D/EF_barra_2_nodos/deduccion_func_forma_EF_barra_2_nodos.m
+++ codigo/1D/EF_barra_2_nodos/deduccion_func_forma_EF_barra_2_nodos.m
@@ -6,7 +6,6 @@ r = solve(u1 == a1*x1 + a0, ...
 disp('a0 = '); pretty(r.a0)
 disp('a1 = '); pretty(r.a1)
 
-u = r.a1*x + r.a0;      % Se define ahora u(x) ya que conocemos a1 y a0
-u = collect(u, u2);     % Se factoriza u2
-u = collect(u, u1);     % Se factoriza u1
-disp('u = '); pretty(u) % Observe aquí las funciones de forma
+u = r.a1*x + r.a0;        % Se define ahora u(x) ya que conocemos a1 y a0
+u = collect(u, [u1, u2]); % Se factoriza u1 y y2
+disp('u = '); pretty(u)   % Observe aquí las funciones de forma
