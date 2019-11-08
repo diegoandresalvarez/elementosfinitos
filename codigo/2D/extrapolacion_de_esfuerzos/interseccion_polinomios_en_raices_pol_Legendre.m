736e83d85368106147dc23ade7e018a0cac4c2e9
Diego Alvarez <daalvarez@unal.edu.co>
2019-11-08 13:44:37 -0500

    Intersección de poliniomio de grado n y n-1

--- codigo/2D/extrapolacion_de_esfuerzos/interseccion_polinomios_en_raices_pol_Legendre.m
+++ codigo/2D/extrapolacion_de_esfuerzos/interseccion_polinomios_en_raices_pol_Legendre.m
@@ -9,18 +9,17 @@ syms x a b              % defino las variables simbolicas
 % el polinomio f y su aproximacion g
 f = 1 + x + x^2;  g = a + b*x;  
 
-E = int((f-g)^2,x,-1,1);        % el ajuste por minimos cuadrados
-r = solve(diff(E,a),...  % = 0
-          diff(E,b),...  % = 0
+E = int((f-g)^2,x,-1,1);  % el ajuste por minimos cuadrados
+r = solve(diff(E,a) == 0,...
+          diff(E,b) == 0,...
           a, b);
      
-g = r.a + r.b*x;             % defino de nuevo la funcion g
-inter = solve(f-g,... % = 0  % calculo la interseccion de ambos polinomios
-              x)
+g = r.a + r.b*x;          % defino de nuevo la funcion g
+inter = solve(f == g, x)  % calculo la interseccion de ambos polinomios
        
-figure; hold on; grid on        % grafico la respuesta
-ezplot(f,[-1 1]); ezplot(g,[-1 1]);
-gg = inline(vectorize(char(g)),'x');
+figure; hold on; grid on  % grafico la respuesta
+fplot([f, g], [-1 1]);
+gg = matlabFunction(g, 'Vars', x);
 plot(inter,gg(inter),'r.','MarkerSize',40);
 title('Interseccion en las raices del polinomio de Legendre de grado 2')
 
@@ -32,17 +31,16 @@ syms x a b c            % defino las variables simbolicas
 f = 1 + x + x^2 + x^3;  g = a + b*x + c*x^2;
 
 E = int((f-g)^2,x,-1,1);        % el ajuste por minimos cuadrados
-r = solve(diff(E,a),...  % = 0
-          diff(E,b),...  % = 0
-          diff(E,c),...  % = 0          
+r = solve(diff(E,a) == 0,...
+          diff(E,b) == 0,...
+          diff(E,c) == 0,...
           a, b, c);
      
 g = r.a + r.b*x + r.c*x^2;   % defino de nuevo la funcion g
-inter = solve(f-g,... % = 0  % calculo la interseccion de ambos polinomios
-              x)
+inter = solve(f == g, x)     % calculo la interseccion de ambos polinomios
        
 figure; hold on; grid on        % grafico la respuesta
-ezplot(f,[-1 1]); ezplot(g,[-1 1]);
-gg = inline(vectorize(char(g)),'x');
+fplot([f, g], [-1 1]);
+gg = matlabFunction(g, 'Vars', x);
 plot(inter,gg(inter),'r.','MarkerSize',40);
 title('Interseccion en las raices del polinomio de Legendre de grado 3')
