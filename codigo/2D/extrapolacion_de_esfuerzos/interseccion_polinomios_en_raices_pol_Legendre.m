646437bfa227a8d1362b4e068a623a781398d908
Diego Alvarez <daalvarez@unal.edu.co>
2019-11-08 13:56:51 -0500

    Intersección de polinomios de GL

--- codigo/2D/extrapolacion_de_esfuerzos/interseccion_polinomios_en_raices_pol_Legendre.m
+++ codigo/2D/extrapolacion_de_esfuerzos/interseccion_polinomios_en_raices_pol_Legendre.m
@@ -1,36 +1,36 @@
 %% Programa para ilustrar que en los puntos de una cuadratura de
-%% Gauss-Legendre de orden n, un polinimio de grado n y otro de grado n-1
+%% Gauss-Legendre de orden n, un polinomio de grado n y otro de grado n-1
 %% obtenido como ajuste por minimos cuadrados del anterior, toman el mismo
 %% valor.
 
-clear, clc, close all   % borro la memoria, la pantalla y cierro figuras
-syms x a b              % defino las variables simbolicas
+clear, clc, close all        % borro la memoria, la pantalla y cierro figuras
+syms x a b                   % defino las variables simbolicas
 
 % el polinomio f y su aproximacion g
 f = 1 + x + x^2;  g = a + b*x;  
 
-E = int((f-g)^2,x,-1,1);  % el ajuste por minimos cuadrados
+E = int((f-g)^2,x,-1,1);     % el ajuste por minimos cuadrados
 r = solve(diff(E,a) == 0,...
           diff(E,b) == 0,...
           a, b);
      
-g = r.a + r.b*x;          % defino de nuevo la funcion g
-inter = solve(f == g, x)  % calculo la interseccion de ambos polinomios
+g = r.a + r.b*x;             % defino de nuevo la funcion g
+inter = solve(f == g, x)     % calculo la interseccion de ambos polinomios
        
-figure; hold on; grid on  % grafico la respuesta
+figure; hold on; grid on     % grafico la respuesta
 fplot([f, g], [-1 1]);
 gg = matlabFunction(g, 'Vars', x);
 plot(inter,gg(inter),'r.','MarkerSize',40);
 title('Interseccion en las raices del polinomio de Legendre de grado 2')
 
 %%
-clear              % borro la memoria
-syms x a b c            % defino las variables simbolicas
+clear                        % borro la memoria
+syms x a b c                 % defino las variables simbolicas
 
 % el polinomio f y su aproximacion g
 f = 1 + x + x^2 + x^3;  g = a + b*x + c*x^2;
 
-E = int((f-g)^2,x,-1,1);        % el ajuste por minimos cuadrados
+E = int((f-g)^2,x,-1,1);     % el ajuste por minimos cuadrados
 r = solve(diff(E,a) == 0,...
           diff(E,b) == 0,...
           diff(E,c) == 0,...
@@ -39,7 +39,7 @@ r = solve(diff(E,a) == 0,...
 g = r.a + r.b*x + r.c*x^2;   % defino de nuevo la funcion g
 inter = solve(f == g, x)     % calculo la interseccion de ambos polinomios
        
-figure; hold on; grid on        % grafico la respuesta
+figure; hold on; grid on     % grafico la respuesta
 fplot([f, g], [-1 1]);
 gg = matlabFunction(g, 'Vars', x);
 plot(inter,gg(inter),'r.','MarkerSize',40);
