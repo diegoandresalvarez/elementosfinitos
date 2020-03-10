69207c6b329287d1bfe9e709569f4dfa6db3f764
Diego Andrés Alvarez Marín <diegotorquemada@gmail.com>
2020-03-10 09:18:57 -0500

    Comentando el código

--- codigo/vigas/Timoshenko/c4_interpolacion_acoplada_ej_2_2.m
+++ codigo/vigas/Timoshenko/c4_interpolacion_acoplada_ej_2_2.m
@@ -10,28 +10,40 @@ N2 = poly2sym(polyfit([-1 0 1],[0 1 0],2),xi);   % = (1+xi)*(1-xi);
 N3 = poly2sym(polyfit([-1 0 1],[0 0 1],2),xi);   % = xi*(xi+1)/2;
 
 %% Se definen w y t (w3, t3 es el nodo central)
-w = N1*w1 + N2*w3 + N3*w2;
-t = N1*t1 + N2*t3 + N3*t2;
+% la idea de esta numeracion es que eliminaremos el nodo central y lo
+% expresaremos en funcion de los gdl asociados a los nodos de los lados
+w = N1*w1 + N2*w3 + N3*w2;  % ¡ojo al orden de los terminos!
+t = N1*t1 + N2*t3 + N3*t2;  % ¡ojo al orden de los terminos!
 
 %% Se calcula gxz = A + B*xi + C*xi^2
 gxz = simplify(diff(w,xi)*dxi_dx - t)
 A = feval(symengine, 'coeff', gxz, xi, 0); % Aqui se esta llamando a la 
 B = feval(symengine, 'coeff', gxz, xi, 1); % funcion "coeff" del MUPAD
 C = feval(symengine, 'coeff', gxz, xi, 2);
+% Nota: tambien se pudo haber usado:
+%{
+gxz = simplify(diff(w,xi)*dxi_dx - t)
+tmp = coeffs(gxz, xi);
+A = tmp(1);
+B = tmp(2);
+C = tmp(3);
+%}
 
 %% Los terminos que acompanan a xi^2 y xi deben hacerse igual a cero (B = C = 0)
-sol = solve(B,C, w3,t3);
+% ya que la teoria de Timoshenko asume que gxz es constante
+sol = solve(B==0, C==0, w3,t3);
 disp('w3 = '); disp(sol.w3)
 disp('t3 = '); disp(sol.t3)
 
 %% Y en funcion de w3 y t3 se reescriben w y t
 w = N1*w1 + N2*sol.w3 + N3*w2;
-w = collect(w,w1);
-w = collect(w,w2)
+w = collect(w, {'w1', 'w2'})
 
 t = N1*t1 + N2*sol.t3 + N3*t2;
-t = collect(t,t1);
-t = collect(t,t2)
+t = collect(t, {'t1', 't2'})
 
 %% Se recalcula gxz
 gxz = simplify(diff(w,xi)*dxi_dx - t)
+
+%% bye, bye!
+return
