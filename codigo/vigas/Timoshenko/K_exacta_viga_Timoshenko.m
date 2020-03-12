2d944fb41d7acd14185b02e580f71a4c44fa07c9
Diego Andrés Alvarez Marín <diegotorquemada@gmail.com>
2020-03-12 18:21:33 -0500

    Deduciendo de la matriz K_T exacta la matriz K_EB

--- codigo/vigas/Timoshenko/K_exacta_viga_Timoshenko.m
+++ codigo/vigas/Timoshenko/K_exacta_viga_Timoshenko.m
@@ -4,7 +4,7 @@ clear, clc
 syms q w x L V(x) M(x) t(x) v(x) EI EA GAast
 
 %% Se calcula la matrix de rigidez
-K = sym(zeros(4));
+K_T = sym(zeros(4));
 for i = 1:4
     sol = dsolve(...       
            diff(V,x) == 0,    ... % se definen las ecuaciones diferenciales
@@ -16,20 +16,28 @@ for i = 1:4
            v(L) == (i==3),    ...           
            t(L) == (i==4));
 
-    K(i,:) = [ +subs(sol.V, x, 0), ...  % Yi  se evaluan las 
+    K_T(i,:) = [ +subs(sol.V, x, 0), ...  % Yi  se evaluan las 
                -subs(sol.M, x, 0), ...  % Mi  reacciones verticales
                -subs(sol.V, x, L), ...  % Yj  y los momentos en los
                +subs(sol.M, x, L) ];    % Mj  apoyos
 end
 beta = (12 * EI)/(L^2 * GAast);
 coef1 = EI/((1 + beta)*L^3);
-disp('K = (EI/((1 + beta)*L^3)) * ');
-pretty(simplify(K/coef1))
+disp('K_T = (EI/((1 + beta)*L^3)) * ');
+pretty(simplify(K_T/coef1))
 % Nota: se puede demostrar que:
 % K22 = K44 = simplify((4+beta)*L^2)
 % K24 = K42 = simplify((2-beta)*L^2)
 
+% Observe que cuando GAast -> Inf, la matriz de rigidez K se vuelve la 
+% misma matriz de rigidez K de la teoría de Euler-Bernoulli:
+K_EB = limit(K_T, GAast, inf);
+coef2 = EI/(L^3);
+disp('K_EB = (EI/(L^3)) * ');
+pretty(simplify(K_EB/coef2))
+
 %% Se calculan las fuerzas nodales equivalentes:
+% q = w;
 q = -w*x/L;
 sol = dsolve(...       
        diff(V,x) == q,    ... % se definen las ecuaciones diferenciales
@@ -45,6 +53,11 @@ f = [ -subs(sol.V, x, 0)   % Yi  se evaluan las reacciones verticales y los
       +subs(sol.M, x, 0)   % Mi  momentos en los apoyos y se les multiplica
       +subs(sol.V, x, L)   % Yj  por -1 para estimar la fuerza nodal 
       -subs(sol.M, x, L) ];% Mj  equivalente
-coef2 = GAast*L^2  + 12*EI;
-disp('f = 1/(GAast*L^2  + 12*EI) * ');
-pretty(simplify(f*coef2))
+
+clear beta
+syms beta
+f = simplify(subs(f, GAast, (12 * EI)/(L^2 * beta)));
+disp('f = ');
+pretty(f)
+
+%% bye, bye!
