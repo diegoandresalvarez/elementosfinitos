1a528d269cbcea3d0ab49bbac815397f9326457e
Diego Andrés Alvarez Marín <diegotorquemada@gmail.com>
2020-03-13 07:30:17 -0500

    Carga trapezoidal

---
+++ codigo/vigas/Euler-Bernoulli/f_carga_trapezoidal.m
@@ -0,0 +1,44 @@
+% Programa para encontrar las cargas nodales equivalentes de:
+%   /|                                      ^  q(x)
+%   /|                             ^  |  |  |                   
+%   /|                    ^  |  |  |  |  |  |  |\
+%   /|           ^  |  |  |  |  |  |  |  |  |  |\
+%   /|  ^  |  |  |  |  |  |  |  |  |  |  |  |  |\
+%   /|  |  |  |  |  |  |  |  |  |  |  |  |  |  |\  
+%   /|  |  |  |  |  |  |  |  |  |  |  |  |  |  |\
+%   /|  |  |  |  |  |  |  |  |  |  |  |  |  |  |\
+%   /|#########################################|\
+%   /|                                         |\
+%
+%    |--------------------L--------------------|
+% 
+% para una viga de Euler-Bernoulli, asumiendo E, I, A constante 
+
+clear, clc
+
+q = (w2-w1)*x/L + w1;       % carga trapezoidal
+syms syms w x L w1 w2 V(x) M(x) t(x) v(x) EI
+sol = dsolve(...       
+       diff(V) == q,    ... % se definen las ecuaciones diferenciales
+       diff(M) == V,    ...
+       diff(t) == M/EI, ... 
+       diff(v) == t,    ...
+       v(0) == 0,       ... % con sus respectivas condiciones de frontera
+       v(L) == 0,       ...
+       t(0) == 0,       ...   
+       t(L) == 0);
+      
+disp('q = '); disp(simplify(q));
+disp('V = '); disp(simplify(sol.V));
+disp('M = '); disp(simplify(sol.M));
+disp('t = '); disp(simplify(sol.t));
+disp('v = '); disp(simplify(sol.v));
+
+% Se evaluan las cargas nodales equivalentes
+disp('Y1 = '); disp(simplify(-subs(sol.V, x, 0)));
+disp('Y2 = '); disp(simplify(+subs(sol.V, x, L)));
+
+% Y los momentos nodales equivalentes
+disp('M1 = '); disp(simplify(+subs(sol.M, x, 0)));
+disp('M2 = '); disp(simplify(-subs(sol.M, x, L)));
+      
