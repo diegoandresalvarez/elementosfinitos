08dbc64eb2f6f44737cb5f10b0cbd07a2dc11224
Diego Alvarez <daalvarez@unal.edu.co>
2020-05-21 22:22:35 -0500

    Organizando archivos Kirchhoff-Love

--- codigo/losas/Kirchhoff_Love/func_forma_MZC.m
+++ codigo/losas/Kirchhoff_Love/func_forma_MZC.m
@@ -1,55 +1,49 @@
-% Calculo de las funciones de forma del elemento placa MZC
-
 clear, clc, close all
 
-syms x y xi eta a b x0 y0 E nu t q
+syms xi eta a b E nu t q
 
-PT = [ 1 x y x^2 x*y y^2 x^3 x^2*y x*y^2 y^3 x^3*y x*y^3 ];
-PT_x = diff(PT,x);
-PT_y = diff(PT,y);
+%% Se calculan las funciones de forma del elemento placa MZC
+pT = [ 1 xi eta xi^2 xi*eta eta^2 xi^3 xi^2*eta xi*eta^2 eta^3 xi^3*eta xi*eta^3 ];
+pT_xi  = diff(pT,xi);
+pT_eta = diff(pT,eta);
 
 A = [ ...
-      subs(PT,   {x, y}, {-1, -1})
-      subs(PT_x, {x, y}, {-1, -1}) % nodo 1
-      subs(PT_y, {x, y}, {-1, -1})
+      subs(pT,     {xi, eta}, {-1, -1})
+      subs(pT_xi,  {xi, eta}, {-1, -1}) % nodo 1
+      subs(pT_eta, {xi, eta}, {-1, -1})
       
-      subs(PT,   {x, y}, { 1, -1})
-      subs(PT_x, {x, y}, { 1, -1}) % nodo 2
-      subs(PT_y, {x, y}, { 1, -1})
+      subs(pT,     {xi, eta}, { 1, -1})
+      subs(pT_xi,  {xi, eta}, { 1, -1}) % nodo 2
+      subs(pT_eta, {xi, eta}, { 1, -1})
       
-      subs(PT,   {x, y}, { 1,  1})
-      subs(PT_x, {x, y}, { 1,  1}) % nodo 3
-      subs(PT_y, {x, y}, { 1,  1})
+      subs(pT,     {xi, eta}, { 1,  1})
+      subs(pT_xi,  {xi, eta}, { 1,  1}) % nodo 3
+      subs(pT_eta, {xi, eta}, { 1,  1})
       
-      subs(PT,   {x, y}, {-1,  1})
-      subs(PT_x, {x, y}, {-1,  1}) % nodo 4
-      subs(PT_y, {x, y}, {-1,  1}) ];
-
-N = PT/A;  % N = PT*inv(A);
-
-% reescribo las funciones de forma en terminos de xi y eta
-N = subs(N,{x, y},{xi, eta}); 
-NN_ = N;
+      subs(pT,     {xi, eta}, {-1,  1})
+      subs(pT_xi,  {xi, eta}, {-1,  1}) % nodo 4
+      subs(pT_eta, {xi, eta}, {-1,  1}) ];
+  
+N = pT/A;  % N = pT*inv(A);
+Nvec = N;
 
 % Reorganizo las funciones de forma en una matriz de 4x3
 % donde las filas representan el nodo i
-N = [ ...
+N = simplify([ ...
       N(1)  N(2)  N(3)
       N(4)  N(5)  N(6)       % esto es lo mismo que reshape(N,3,4).'
       N(7)  N(8)  N(9)
-      N(10) N(11) N(12) ];
+      N(10) N(11) N(12) ]);
 
 disp('Las funciones de forma son =')
 for i = 1:4
    fprintf('N{%d}   = \n',i); pretty(N(i,1));
    fprintf('Nb{%d}  = \n',i); pretty(N(i,2));
    fprintf('Nbb{%d} = \n',i); pretty(N(i,3));
-end;
+end
 
 %% Grafico las funciones de forma
-XXI  = -1:0.05:1;
-EETA = -1:0.05:1;
-[XI,ETA] = meshgrid(XXI,EETA);
+[XI, ETA] = meshgrid(-1:0.05:1);
 
 for i = 1:4
    figure                    % Creo un lienzo
@@ -58,26 +52,17 @@ for i = 1:4
       grid on                % creo la rejilla
       hold on;               % Para que no se sobreescriban los graficos
 
-      % con este comando convierto la funcion de forma de tipo simbolico a
-      % tipo funcion
-      % funcion_texto = char(N(i,j));  % convierto la cadena a texto
-      % funcion_texto = strrep(funcion_texto,'*','.*'); % reemplazo * por .*
-      % funcion_texto = strrep(funcion_texto,'^','.^'); % reemplazo ^ por .^
+      NN = matlabFunction(N(i,j), 'Vars', {'xi','eta'});
 
-      funcion_texto = vectorize(char(N(i,j)));
-
-      NN = inline(funcion_texto,'xi','eta'); % cadena a funcion inline
-      % se recomienda aqui mirar la ayuda de la funcion inline
-      
-      xlabel('\xi', 'FontSize',26); % titulo eje X
-      ylabel('\eta','FontSize',26); % titulo eje Y
+      xlabel('\xi', 'FontSize',16); % titulo eje X
+      ylabel('\eta','FontSize',16); % titulo eje Y
       switch j % imprimo el titulo
          case 1
-            title(sprintf('N_{%d}(\\xi,\\eta)',       i),'FontSize',26);
+            title(sprintf('N_{%d}(\\xi,\\eta)',       i),'FontSize',16);
          case 2
-            title(sprintf('(N_b)_{%d}(\\xi,\\eta)',   i),'FontSize',26);
+            title(sprintf('(N_b)_{%d}(\\xi,\\eta)',   i),'FontSize',16);
          case 3
-            title(sprintf('(N_{bb})_{%d}(\\xi,\\eta)',i),'FontSize',26);
+            title(sprintf('(N_{bb})_{%d}(\\xi,\\eta)',i),'FontSize',16);
       end
       mesh(XI, ETA, NN(XI,ETA),'LineWidth',2); % malla de alambre
       surf(XI, ETA, NN(XI,ETA));               % superficie
@@ -88,26 +73,26 @@ for i = 1:4
       axis tight             % ejes apretados
       daspect([1 1 1]);      % similar a axis equal pero en 3D
       view(3);               % vista tridimensional
-   end;
-end;
+   end
+end
 drawnow; % vaciar el buffer de graficos de MATLAB antes de continuar
 
 %% Calculo de la matriz de deformacion
 BB = cell(4,1);
 for i = 1:4
-   BB{i} = [...
-    -diff(N(i,1),xi,2)/(a^2)           -diff(N(i,2),xi,2)/(a^2)           -diff(N(i,3),xi,2)/(a^2)
-    -diff(N(i,1),eta,2)/(b^2)          -diff(N(i,2),eta,2)/(b^2)          -diff(N(i,3),eta,2)/(b^2)      
-    -2*diff(diff(N(i,1),xi),eta)/(a*b) -2*diff(diff(N(i,2),xi),eta)/(a*b) -2*diff(diff(N(i,3),xi),eta)/(a*b) ];
-end;
-B = simple([BB{1} BB{2} BB{3} BB{4}]);
+   BB{i} = -[...
+       diff(N(i,1),xi, 2)/(a^2)    diff(N(i,2),xi, 2)/(a^2)    diff(N(i,3),xi, 2)/(a^2)
+       diff(N(i,1),eta,2)/(b^2)    diff(N(i,2),eta,2)/(b^2)    diff(N(i,3),eta,2)/(b^2)      
+    2*diff(N(i,1),xi,eta)/(a*b) 2*diff(N(i,2),xi,eta)/(a*b) 2*diff(N(i,3),xi,eta)/(a*b) ];
+end
+Bb = simplify([BB{1} BB{2} BB{3} BB{4}]);
 
 % matriz constitutiva
-D = E/(1-nu^2)* [ 1  nu 0
-                  nu 1  0
-                  0  0  (1-nu)/2 ];
+D = E/(1-nu^2) * [ 1  nu 0
+                   nu 1  0
+                   0  0  (1-nu)/2 ];
                
-Df = (t^3/12)*D; % matriz constitutiva de flexion generalizada
+Db = (t^3/12)*D; % matriz constitutiva de flexion generalizada
 
 %% Calculo la matriz de rigidez
 %{
@@ -120,18 +105,16 @@ por lo tanto:
 %}
 det_J = a*b;
 
-tic
-disp ('Calculando la matriz de rigidez: espere aproximadamente dos minutos');
-K = simple(int(int(B.'*Df*B*det_J, xi, -1, 1), eta, -1, 1))
-toc
+disp ('Calculando la matriz de rigidez: espere aproximadamente dos minutos (en mi PC de 2010)');
+K = simplify(int(int(Bb.'*Db*Bb*det_J, xi, -1, 1), eta, -1, 1))
 
-disp ('Calculando la matriz Df*B');
-Df_B = simple(Df*B)
+disp ('Calculando la matriz Db*Bb');
+Db_B = simplify(Db*Bb)
 
 disp ('Calculando el vector de fuerzas nodales equivalentes');
-f = int(int(N*q*det_J,xi,-1,1),eta,-1,1);
+f = int(int(N*q*det_J, xi,-1,1), eta,-1,1);
 disp('f = 4*q*a*b*')
-disp(simple(f/(4*q*a*b)))
+disp(simplify(f/(4*q*a*b)))
 %{
 f = 4*q*a*b*
 [ 1/4,  1/12,  1/12]
@@ -140,10 +123,11 @@ f = 4*q*a*b*
 [ 1/4,  1/12, -1/12]
 %}
 
+
 % Calculo de la matriz de masa consistente (FALTA VERIFICAR)
 syms rho
 disp ('Calculando la matriz de masa consistente');
-M = simple(int(int(rho*NN_.'*NN_*det_J,xi,-1,1),eta,-1,1));
+M = simplify(int(int(rho*Nvec.'*Nvec*det_J, xi,-1,1), eta,-1,1));
 disp('M = (a*b*rho)*');
 disp(M/(a*b*rho))
 
@@ -152,7 +136,7 @@ return %bye, bye!
 %% LA MATRIZ DE RIGIDEZ (copie y pegue en su codigo)
 %{
 DD = E*t^3/(12*(1-nu^2));   % rigidez a flexion de la placa
-Ksimple = simple(K*(a*b)/DD)
+Ksimplify = simplify(K*(a*b)/DD)
 
 Obteniendo:
 DD = E*t^3/(12*(1-nu^2));   % rigidez a flexion de la placa
@@ -171,8 +155,9 @@ K = DD/(a*b)*[ ...
                   a^2/b^2 - nu/10 + 1/10,                                  0,     nu/15 + (2*a^2)/(3*b^2) - 1/15,              nu/10 + a^2/(2*b^2) - 1/10,                                  0,         a^2/(3*b^2) - nu/15 + 1/15,           (2*nu)/5 - a^2/(2*b^2) + 1/10,                                  0, (4*nu)/15 + (2*a^2)/(3*b^2) - 4/15,             - (2*nu)/5 - a^2/b^2 - 1/10,                                -nu, (4*a^2)/(3*b^2) - (4*nu)/15 + 4/15 ];
 
 y
-Df_B_simple = simple(Df_B*4/DD)
-Df_B = DD/4*[...  % = Df*B
+
+Db_Bb_simplify = simplify(Df_B*4/DD)
+Db_Bb = DD/4*[...  % = Df*B
   (3*eta*nu*(xi - 1))/b^2 - (3*xi - 3*eta*xi)/a^2,              ((3*xi - 1)*(eta - 1))/a^2,             (nu*(3*eta - 1)*(xi - 1))/b^2,    (3*xi - 3*eta*xi)/a^2 - (3*eta*nu*(xi + 1))/b^2,              ((3*xi + 1)*(eta - 1))/a^2,           -(nu*(3*eta - 1)*(xi + 1))/b^2,  (3*xi + 3*eta*xi)/a^2 + (3*eta*nu*(xi + 1))/b^2,            -((3*xi + 1)*(eta + 1))/a^2,           -(nu*(3*eta + 1)*(xi + 1))/b^2, - (3*xi + 3*eta*xi)/a^2 - (3*eta*nu*(xi - 1))/b^2,            -((3*xi - 1)*(eta + 1))/a^2,             (nu*(3*eta + 1)*(xi - 1))/b^2
  (3*nu*xi*(eta - 1))/a^2 - (3*eta - 3*eta*xi)/b^2,           (nu*(3*xi - 1)*(eta - 1))/a^2,                ((3*eta - 1)*(xi - 1))/b^2, - (3*eta + 3*eta*xi)/b^2 - (3*nu*xi*(eta - 1))/a^2,           (nu*(3*xi + 1)*(eta - 1))/a^2,              -((3*eta - 1)*(xi + 1))/b^2, (3*eta + 3*eta*xi)/b^2 + (3*nu*xi*(eta + 1))/a^2,         -(nu*(3*xi + 1)*(eta + 1))/a^2,              -((3*eta + 1)*(xi + 1))/b^2,  (3*eta - 3*eta*xi)/b^2 - (3*nu*xi*(eta + 1))/a^2,         -(nu*(3*xi - 1)*(eta + 1))/a^2,                ((3*eta + 1)*(xi - 1))/b^2
        -((nu - 1)*(3*eta^2 + 3*xi^2 - 4))/(2*a*b), -((3*xi + 1)*(nu - 1)*(xi - 1))/(2*a*b), -((3*eta + 1)*(eta - 1)*(nu - 1))/(2*a*b),          ((nu - 1)*(3*eta^2 + 3*xi^2 - 4))/(2*a*b), -((3*xi - 1)*(nu - 1)*(xi + 1))/(2*a*b), ((3*eta + 1)*(eta - 1)*(nu - 1))/(2*a*b),       -((nu - 1)*(3*eta^2 + 3*xi^2 - 4))/(2*a*b), ((3*xi - 1)*(nu - 1)*(xi + 1))/(2*a*b), ((3*eta - 1)*(eta + 1)*(nu - 1))/(2*a*b),         ((nu - 1)*(3*eta^2 + 3*xi^2 - 4))/(2*a*b), ((3*xi + 1)*(nu - 1)*(xi - 1))/(2*a*b), -((3*eta - 1)*(eta + 1)*(nu - 1))/(2*a*b) ];
