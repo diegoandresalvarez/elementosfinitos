a3aeddf5ac8a93dff2b7580367d3eae0d9c2c316
Diego Andres Alvarez Marin <daalvarez@unal.edu.co>
2020-09-10 16:15:46 -0500

    Mejorando comentarios

--- codigo/repaso_matricial/cercha_2d/ejemplo_cercha_apoyo_inclinado.m
+++ codigo/repaso_matricial/cercha_2d/ejemplo_cercha_apoyo_inclinado.m
@@ -3,28 +3,28 @@
 %% Unidades en toneladas y cm
 
 %% defino las variables
-ang   = atan2(300,400)*180/pi; % angulo especificado en grados
+ang   = atan2d(300,400); % angulo especificado en grados
 
 %barra    1       2       3       4       5
 theta = [ ang     0       -ang    0       -90  ]; % angulo de inclinacion 
-long  = [ 500     400     500     400      300 ]; % longitud barra
-area  = [ 100     40      150     40       30  ]; % area barra
+L     = [ 500     400     500     400      300 ]; % longitud barra
+A     = [ 100     40      150     40       30  ]; % area barra
 
 % LaG: local a global: matriz que relaciona nodos locales y globales
-LaG = [1 3;   % fila = barra
-       1 4;   % col1 = nodo global asociado a nodo local 1
-       3 2;   % col2 = nodo global asociado a nodo local 2
-       4 2; 
+LaG = [1 3    % fila = barra
+       1 4    % col1 = nodo global asociado a nodo local 1
+       3 2    % col2 = nodo global asociado a nodo local 2
+       4 2   
        3 4 ]; % (se lee la barra x va del nodo i al nodo j)
 
 % gdl: grados de libertad
-gdl   = [1 2;  % fila = nodo
-         3 4;  % col1 = gdl en direccion x
-         5 6;  % col2 = gdl en direccion y
-         7 8]; 
+gdl = [ 1 2   % fila = nodo
+        3 4   % col1 = gdl en direccion x
+        5 6   % col2 = gdl en direccion y
+        7 8 ]; 
 
-E = 2040; %ton/cm^2
-k = E.*area./long;      % rigidez de cada barra
+E = 2040;     % ton/cm^2
+k = E.*A./L;  % rigidez de cada barra
 
 %% ensamblo la matriz de rigidez global
 Kg = sparse(8,8); T = cell(5,1); % separo memoria
@@ -35,12 +35,12 @@ for e = 1:5  % para cada barra
            -s  c  0  0         % para la barra e
             0  0  c  s 
             0  0 -s  c ];
-   Kloc = k(e)*[ 1  0 -1  0;   % matriz de rigidez local expresada en el 
-                 0  0  0  0;   % sistema de coordenadas locales para la                  
-                -1  0  1  0;   % barra e
-                 0  0  0  0 ];
+   Kloc = [ k(e)  0 -k(e)  0    % matriz de rigidez local expresada en el 
+            0  0  0        0    % sistema de coordenadas locales para la                  
+           -k(e)  0  k(e)  0    % barra e
+            0     0  0     0 ];
    Kg(idx,idx) = Kg(idx,idx) + T{e}'*Kloc*T{e}; % sumo a K global
-end;
+end
 
 %% introduciendo los soportes inclinados
 Tg = speye(8,8);
@@ -76,7 +76,7 @@ f  = Tg*fg;
 fc = f(d);
 
 %% resuelvo el sistema de ecuaciones
-ad = Kdd\(fc-Kdc*ac); % = linsolve(Kdd, fc-Kdc*ac)
+ad = Kdd\(fc - Kdc*ac); % = linsolve(Kdd, fc - Kdc*ac)
 qd = Kcc*ac + Kcd*ad;
 
 % armo los vectores de desplazamientos (a) y fuerzas (q)
@@ -89,12 +89,12 @@ a(d) = ad;     % q(d) = qc = 0
 qg = Tg'*q;
 ag = Tg'*a;
 
-%% calculo las cargas axiales (N) en cada barra
-N = zeros(5,1);
+%% calculo las fuerzas axiales (fax) en cada barra
+fax = zeros(5,1);
 for e = 1:5 % para cada barra
    idx = [gdl(LaG(e,1),:) gdl(LaG(e,2),:)]; % saco los 4 gdls de la barra e
-   N(e) = k(e)*[-1 0 1 0]*T{e}*ag(idx);
-end;
+   fax(e) = [-k(e) 0 k(e) 0]*T{e}*ag(idx);
+end
 
 %% imprimo los resultados
-ag, qg, N
+ag, qg, fax
