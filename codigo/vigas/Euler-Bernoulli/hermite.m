7e4ed8390420538dc6d41660c1c96fe886d26dd4
Diego Andres Alvarez Marin <daalvarez@unal.edu.co>
2021-02-26 13:42:52 -0500

    Mejorando la presentacion del codigo

--- codigo/vigas/Euler-Bernoulli/hermite.m
+++ codigo/vigas/Euler-Bernoulli/hermite.m
@@ -6,23 +6,23 @@ syms xi u1 u2 u1p u2p
 x1 = -1; x2 =  1;
 
 %% Funciones de forma Lagrangianas
-L1 = poly2sym(polyfit([-1 1],[1 0],1),xi);
-L2 = poly2sym(polyfit([-1 1],[0 1],1),xi);
+L1 = poly2sym(polyfit([-1 1], [1 0], 1), xi);
+L2 = poly2sym(polyfit([-1 1], [0 1], 1), xi);
 
 %% Se calculan los polinomios Pi y Qi
-P1 = (1 - 2*subs(diff(L1,xi),xi,x1)*(xi-x1))*L1^2;
-P2 = (1 - 2*subs(diff(L2,xi),xi,x2)*(xi-x2))*L2^2;
-Q1 = (xi-x1)*L1^2; 
-Q2 = (xi-x2)*L2^2;
+P1 = (1 - 2*subs(diff(L1, xi), xi, x1)*(xi - x1))*L1^2;
+P2 = (1 - 2*subs(diff(L2, xi), xi, x2)*(xi - x2))*L2^2;
+Q1 = (xi - x1)*L1^2; 
+Q2 = (xi - x2)*L2^2;
 
 %% Se calcula el polinomio interpolador
 u = simplify(P1*u1 + P2*u2 + Q1*u1p + Q2*u2p);
 
 %% Se calculan las funciones de forma
-N1  = expand(subs(u, {u1, u1p, u2, u2p} , {1, 0, 0, 0})); % = P1
-N1b = expand(subs(u, {u1, u1p, u2, u2p} , {0, 1, 0, 0})); % = Q1
-N2  = expand(subs(u, {u1, u1p, u2, u2p} , {0, 0, 1, 0})); % = P2
-N2b = expand(subs(u, {u1, u1p, u2, u2p} , {0, 0, 0, 1})); % = Q2
+N1  = expand(subs(u, {u1, u1p, u2, u2p}, {1, 0, 0, 0})); % = P1
+N1b = expand(subs(u, {u1, u1p, u2, u2p}, {0, 1, 0, 0})); % = Q1
+N2  = expand(subs(u, {u1, u1p, u2, u2p}, {0, 0, 1, 0})); % = P2
+N2b = expand(subs(u, {u1, u1p, u2, u2p}, {0, 0, 0, 1})); % = Q2
 
 %% Se muestran los resultados
 disp('N_1(xi)  = '); pretty(N1);    
