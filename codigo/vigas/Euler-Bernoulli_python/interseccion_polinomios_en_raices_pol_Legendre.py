908d43ffe0f682b778cfefc2203818659ce82124
Diego Andres Alvarez Marin <daalvarez@unal.edu.co>
2021-03-12 11:23:22 -0500

    Mejorando la claridad

--- codigo/vigas/Euler-Bernoulli_python/interseccion_polinomios_en_raices_pol_Legendre.py
+++ codigo/vigas/Euler-Bernoulli_python/interseccion_polinomios_en_raices_pol_Legendre.py
@@ -2,14 +2,15 @@
 
 # Programa para ilustrar que en los puntos de una cuadratura de
 # Gauss-Legendre de orden n, un polinomio de grado n y otro de grado n-1
-# obtenido como ajuste por minimos cuadrados del anterior, toman el mismo
+# obtenido como ajuste por mínimos cuadrados del anterior, toman el mismo
 # valor.
 
 # %%Importación de librerías
 
 import sympy as sp
 import numpy as np
-import matplotlib.pyplot as plt
+from sympy.plotting import plot
+
 
 # %%Interseccion en las raices del polinomio de Legendre de grado 2.
 
@@ -20,35 +21,43 @@ x, a, b = sp.symbols('x a b')
 f = 1 + x + x**2
 g = a + b*x
 
-E = sp.integrate((f-g)**2, (x, -1, 1))  # El ajuste por minimos cuadrados
+E = sp.integrate((f-g)**2, (x, -1, 1))  # Ajuste por mínimos cuadrados
 r = sp.solve([sp.diff(E, a),            # diff(E,a) = 0
               sp.diff(E, b)],           # diff(E,b) = 0
              [a, b])
 
 g = r[a] + r[b]*x         # Defino de nuevo la funcion g
-inter = np.array(sp.solve(f-g, x))  # Cálculo la interseccion de los polinomios
+inter = sp.solve(f-g, x)  # Calculo la interseccion de los polinomios
 
 # Gráfica
-fcal = sp.lambdify([x], f)        # Expresión en forma de función
-gcal = sp.lambdify([x], g)
-xcal = np.arange(-1, 1, 0.05)  # Puntos de evaluación de las funciones.
+p = plot(f, g, (x, -1, 1), 
+         legend=True, 
+         title='Intersección en las raices del polinomio de Legendre de grado 2',
+         xlabel=r'$\xi$',
+         ylabel='',
+         aspect_ratio = (1,1),
+         show=False)
+p[0].line_color = 'r'; p[0].label=r'$f(\xi)$'
+p[1].line_color = 'b'; p[1].label=r'$g(\xi)$'
 
-fig, ax = plt.subplots(figsize=(6, 6))
+x1, f1 = float(inter[0]), float(f.subs(x, inter[0]))
+x2, f2 = float(inter[1]), float(f.subs(x, inter[1]))
+p.annotations = [
+{
+  's'    : f'({x1:.4f}, {f1:.4f})',  # Nota en las versiones nuevas es 'text' en vez de 's'
+  'xy'   : (x1 - 0.41, f1 + 0.21),
+},
+{
+  's'    : f'({x2:.4f}, {f2:.4f})',
+  'xy'   : (x2 - 0.41, f2 + 0.21),
+}
+]
+p.show()
 
-ax.plot(xcal, fcal(xcal), color='b')
-ax.plot(xcal, gcal(xcal), color='r')
-ax.plot(inter, fcal(inter), 'o', color='r', ms=10)
-for i in range(2):
-    ax.annotate(f'({round(inter[i].evalf(),2)}, '
-                + f'{round(fcal(inter[i].evalf()),2)})',
-                xy=(round(inter[i].evalf(), 2) - 0.41,
-                    round(fcal(inter[i].evalf()), 2) + 0.21),
-                fontsize=13)
-
-ax.set_title('Intersección en las raices del polinomio de Legendre de grado 2')
-ax.grid()
 
 # %%Interseccion en las raices del polinomio de Legendre de grado 3.
+import matplotlib.pyplot as plt
+
 
 # Se definen las variables simbólicas
 x, a, b, c = sp.symbols('x a b c')
@@ -67,24 +76,23 @@ g = r[a] + r[b]*x + r[c]*x**2  # Defino de nuevo la funcion g
 inter = np.array(sp.solve(f-g, x))  # Cálculo la interseccion de los polinomios
 
 # Gráfica
-fcal = sp.lambdify([x], f)        # Expresión en forma de función
-gcal = sp.lambdify([x], g)
-xcal = np.arange(-1, 1, 0.05)  # Puntos de evaluación de las funciones.
+f = sp.lambdify([x], f)        # Expresión en forma de función
+g = sp.lambdify([x], g)
+x = np.arange(-1, 1, 0.05)  # Puntos de evaluación de las funciones.
 
 fig, ax = plt.subplots(figsize=(6, 6))
 
-ax.plot(xcal, fcal(xcal), color='b')
-ax.plot(xcal, gcal(xcal), color='r')
-ax.plot(inter, fcal(inter), 'o', color='r', ms=10)
+ax.plot(x, f(x), color='b', label=r'$f(\xi)$')
+ax.plot(x, g(x), color='r', label=r'$g(\xi)$')
+ax.plot(inter, f(inter), 'o', color='r', ms=10)
 for i in range(3):
-    ax.annotate(f'({round(inter[i].evalf(),2)}, '
-                + f'{round(fcal(inter[i].evalf()),2)})',
-                xy=(round(inter[i].evalf(), 2) - 0.3,
-                    round(fcal(inter[i].evalf()), 2) + 0.3),
-                fontsize=13)
+    xx, fx = float(inter[i]), float(f(inter[i]))
+    ax.annotate(f'({xx:.4f}, {fx:.4f})', xy=(xx - 0.3, fx + 0.3))
 
 ax.set_title('Intersección en las raices del polinomio de Legendre de grado 3')
+ax.set_xlabel(r'$\xi$')
+ax.legend()
 ax.grid()
 plt.show()
 
-# Fin del programa.
+#%% Fin del programa.
