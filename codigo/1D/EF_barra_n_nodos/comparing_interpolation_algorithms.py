b63018b980565c56bf568f21cea561c36e1379b9
Diego Andrés Alvarez Marín <daalvarez@unal.edu.co>
2025-06-04 12:40:47 -0500

    Adding Akima+Makima

--- codigo/1D/EF_barra_n_nodos/comparing_interpolation_algorithms.py
+++ codigo/1D/EF_barra_n_nodos/comparing_interpolation_algorithms.py
@@ -1,20 +1,38 @@
 # Interpolation algorithms already implemented in SCIPY
 #
 # WHO   DATE            WHAT
-# DAA   Oct 6, 2020      First algorithm
+# DAA   Oct  6, 2020      First algorithm
+# DAA   May 30, 2025      Added Akima and Makima
 #
-# DAA - Diego Andres Alvarez Marin - diegoandresalvarez@gmx.net
+# DAA - Diego Andres Alvarez Marin - daalvarez@unal.edu.co
 
 import numpy as np
 import matplotlib.pyplot as plt
 # %matplotlib qt5
-import scipy.interpolate as interpolate
-from numpy.polynomial.polynomial import Polynomial
+from scipy.interpolate import (
+    PchipInterpolator, 
+    CubicSpline, 
+    Akima1DInterpolator, 
+    interp1d, 
+    lagrange
+)    
+# This script shows how to use different interpolation algorithms
+# to interpolate a set of points in 1D. 
+
+# The user clicks on the plot to input the data points.
+# The algorithms are:
+# - PchipInterpolator         : Piecewise Cubic Hermite Interpolating Polynomial
+# - CubicSpline               : Cubic spline interpolation
+# - Akima1DInterpolator       : Akima's method for 1D interpolation
+# - interp1d with 'nearest'   : Nearest neighbor interpolation
+# - interp1d with 'linear'    : Linear interpolation
+# - interp1d with 'quadratic' : Quadratic interpolation
+# - interp1d with kind=5      : Polynomial interpolation of degree 5
+# - lagrange                  : Lagrange polynomial interpolation
 
 
 # Input the data points
 n = 10           #number of data points
-
 plt.figure()
 plt.title(f'Click with the mouse {n} times. The interpolation will pass through those points')
 plt.axis([-5, 5, -5, 5])
@@ -23,12 +41,16 @@ x, y = pts[:,0], pts[:,1]
 plt.close()
 
 algorithms = [
-(interpolate.interp1d(x, y, kind='nearest',   fill_value='extrapolate'), 'nearest'  ),
-(interpolate.interp1d(x, y, kind='linear',    fill_value='extrapolate'), 'linear'   ),
-(interpolate.interp1d(x, y, kind='quadratic', fill_value='extrapolate'), 'quadratic'),
-(interpolate.interp1d(x, y, kind='cubic',     fill_value='extrapolate'), 'cubic'    ),
-(interpolate.interp1d(x, y, kind=5,           fill_value='extrapolate'), '5'        ),
-(interpolate.lagrange(x,y),                                              'Lagrange' ),
+    (PchipInterpolator(  x, y,                       extrapolate=True),         'Pchip'      ),
+    (CubicSpline(        x, y, bc_type='not-a-knot', extrapolate=True),         'CubicSpline'),
+    (Akima1DInterpolator(x, y, method='akima'),                                 'Akima'      ),
+    (Akima1DInterpolator(x, y, method='makima'),                                'Makima'     ),
+    (interp1d(           x, y, kind='nearest',       fill_value='extrapolate'), 'nearest'    ),
+    (interp1d(           x, y, kind='linear',        fill_value='extrapolate'), 'linear'     ),
+    (interp1d(           x, y, kind='quadratic',     fill_value='extrapolate'), 'quadratic'  ),
+#   (interp1d(           x, y, kind='cubic',         fill_value='extrapolate'), 'cubic'      ), # same as CubicSpline
+    (interp1d(           x, y, kind=5,               fill_value='extrapolate'), '5'          ),
+    (lagrange(           x,y),                                                  'Lagrange'   ),
 # There are many more functions, as documented in:
 # https://docs.scipy.org/doc/scipy/reference/interpolate.html
 ]
@@ -49,5 +71,4 @@ plt.legend(loc='best')
 plt.ylim([-5, 5])
 plt.show()
 
-# más ejemplos aquí: https://mmas.github.io/interpolation-scipy
-
+# More examples here: https://mmas.github.io/interpolation-scipy
